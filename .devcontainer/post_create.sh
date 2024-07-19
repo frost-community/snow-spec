b08087e03728ec7122e38b2d2ca00214422c9f33
marihachi <marihachi0620@gmail.com>
2024-07-20 07:03:29 +0900

    add files

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# +----------------------------------------------------------------------------------------+ #
+# |  _____                                          _             _                        | #
+# | |  __ \                                        | |           (_)                       | #
+# | | |  | |   ___  __   __   ___    ___    _ __   | |_    __ _   _   _ __     ___   _ __  | #
+# | | |  | |  / _ \ \ \ / /  / __|  / _ \  | '_ \  | __|  / _` | | | | '_ \   / _ \ | '__| | #
+# | | |__| | |  __/  \ V /  | (__  | (_) | | | | | \ |_  | (_| | | | | | | | |  __/ | |    | #
+# | |_____/   \___|   \_/    \___|  \___/  |_| |_|  \__|  \__,_| |_| |_| |_|  \___| |_|    | #
+# |                                                                                        | #
+# +----------------------------------------------------------------------------------------+ #
+
+# Bash Strict Mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
+
+# Constants
+WORKSPACE_PATH=/workspaces
+BACKEND_PACKAGE_PATH="${WORKSPACE_PATH}/backend"
+FRONTEND_PACKAGE_PATH="${WORKSPACE_PATH}/frontend"
+CUSTOM_POST_CREATE_SCRIPT_PATH="${WORKSPACE_PATH}/.devcontainer/post_create_custom.sh"
+
+# 各パッケージの package.json を参照して pnpm と依存関係をインストールする
+echo "Install backend dependencies..."
+cd "$BACKEND_PACKAGE_PATH"
+corepack pnpm install --frozen-lockfile
+
+echo "Install frontend dependencies..."
+cd "$FRONTEND_PACKAGE_PATH"
+corepack pnpm install --frozen-lockfile
+
+# Run custom post create script
+if [ -f "$CUSTOM_POST_CREATE_SCRIPT_PATH" ]; then
+  echo "Run custom post create script ($CUSTOM_POST_CREATE_SCRIPT_PATH)..."
+  eval "$CUSTOM_POST_CREATE_SCRIPT_PATH"
+fi
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # snow-spec
 A human-friendly, language for describing your API spec that generates OpenAPI file.
+
+## License
+MIT
